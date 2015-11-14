1c37802dfc0b41379cf3f59b4e0b76c4d4ffe126
ebencarek <ebengc@gmail.com>
2015-11-14 15:57:26 -0500

    Facebook login works

--- app/src/main/java/com/brebalki/friendme/Facebook.java
+++ app/src/main/java/com/brebalki/friendme/Facebook.java
@@ -1,8 +1,20 @@
 package com.brebalki.friendme;
 
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+
 /**
  * Created by ebencarek on 11/14/15.
  */
 public class Facebook {
+    public void loginSuccess(LoginResult loginResult) {
+        Log.d("FB", "success\n");
 
+    }
+
+    public void printCancel() {
+        Log.d("FB", "cancel\n");
+    }
 }
--- app/src/main/java/com/brebalki/friendme/MainActivity.java
+++ app/src/main/java/com/brebalki/friendme/MainActivity.java
@@ -1,21 +1,67 @@
 package com.brebalki.friendme;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    CallbackManager callbackManager;
+    Facebook fb = new Facebook();
+    LoginButton loginButton;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Activate facebook sdk
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        // register login callback
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) this.findViewById(R.id.text_view);
+        loginButton = (LoginButton) this.findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        fb.loginSuccess(loginResult);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        fb.printCancel();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -49,5 +100,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /* Facebook app activation and deactivation tracking */
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 }
